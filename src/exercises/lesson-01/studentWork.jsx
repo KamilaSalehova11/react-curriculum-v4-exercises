82f324fe18b1377775e2e902ddf265e613b69ad8
kamila <kamilasalehova11@gmail.com>
2026-04-08 21:10:47 -0400

    done

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,26 @@
 
 export default function StudentWork() {
   //add variables here
+  const name = 'Kamila';
+  const age = 39;
+  const hobbies = ['Reading', 'Coding', 'Baking'];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About Me</h1>
+      <p>
+        {' '}
+        Hi! My name is {name}. I am {age} years old. I learn React with Code the
+        Dream. I enjoy growing my skills in programming and creating new
+        projects.{' '}
+      </p>
+      <h2>My hobbies</h2>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
