b8426eac1ef1ab6a9469f95533c63e9aef7d3049
kamila <kamilasalehova11@gmail.com>
2026-04-25 19:25:08 -0400

    done

--- src/exercises/lesson-03/BugEffectLoop.jsx
+++ src/exercises/lesson-03/BugEffectLoop.jsx
@@ -7,18 +7,19 @@
   The effect is running on every render, which causes the
   component to behave incorrectly.
   */
-
 import { useEffect, useState } from 'react';
 
 export default function BugEffectLoop() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setCount(count + 1);
-  });
+    setCount[count + 1];
+  }, []);
 
   return <p>Bug 1 Count: {count}</p>;
 }
 
 // Explanation:
 // (Write your explanation here)
+// <- add empty brackets here
+//the empty dependency array prevents the effect from running after every render, so the timeout is created only once when the component first appears.
--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -1,3 +1,7 @@
+import BugEffectLoop from './BugEffectLoop';
+import BugMutatedState from './BugMutatedState';
+import BugProps from './BugProps';
+
 //Lesson-03 Component Lifecycle, Hooks, State, and Props
 //Exercise: React Bug Hunt – Fix the broken components in this folder
 //Impport components here
@@ -6,6 +10,11 @@ export default function StudentWork() {
   return (
     <div>
       {/* add components here */}
+      <BugEffectLoop />
+
+      <BugMutatedState />
+
+      <BugProps />
       <p>Student output will go here</p>
     </div>
   );
