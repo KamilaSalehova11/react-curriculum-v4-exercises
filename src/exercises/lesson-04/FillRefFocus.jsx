8d1e8af48a5879be12f56afff23e5bade1b895bc
kamila <kamilasalehova11@gmail.com>
2026-05-01 01:03:53 -0400

    not focusInput() just buil in focus() method

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,10 +1,14 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
 import { useRef } from 'react';
+
 export default function FillRefFocus() {
   const inputRef = useRef();
-  function focusInput() {}
-  inputRef.current.focusInput;
+
+  function focusInput() {
+    inputRef.current.focus();
+  }
+
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
