9ddeb670ca7681955c9f776a94340b5579dfc3cf
kamila <kamilasalehova11@gmail.com>
2026-05-01 00:58:11 -0400

    none

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,8 +1,10 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+import { useRef } from 'react';
 export default function FillRefFocus() {
+  const inputRef = useRef();
   function focusInput() {}
-
+  inputRef.current.focusInput;
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
